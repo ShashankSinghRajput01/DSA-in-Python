e5436d75918f1f8463b0a3a5061e934c27a5fd43
Shashank <kums32115@gmail.com>
2024-09-14 00:06:44 +0530

    Doubly LL

---
+++ DoublyLinkedList.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self,key):
+        self.key=key
+        self.prev=None
+        self.next=None
+head=Node(10)
+temp1=Node(20)
+temp2=Node(30)
+head.next=temp1
+temp1.prev=head
+temp1.next=temp2
+temp2.prev=temp1
